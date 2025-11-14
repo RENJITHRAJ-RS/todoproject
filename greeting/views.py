18bb6ae386d8bf661e2947956ef96542d697b811
RENJITHRAJ-RS <renjithraj911@gmail.com.com>
2025-11-14 19:27:57 +0530

    Implement login form and update greeting view to handle user input

--- greeting/views.py
+++ greeting/views.py
@@ -1,5 +1,12 @@
-from django.shortcuts import render
-def greeting(request):
-    return render(request,'index.html')
+
 
 # Create your views here.
+from django.shortcuts import render
+def greeting(request):
+    if request.GET:
+        email = request.GET.get('email')
+        return render(request,'form-data.html',{
+            'formData':request.GET,
+            'email': email
+        })
+    return render(request,'index.html' )
